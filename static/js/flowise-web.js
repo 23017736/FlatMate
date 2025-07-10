3f18792e352f01c7d8706e6fccfe04a33689e500
NUR UMAIRAH BINTE IRWAN <23017736@myrp.edu.sg>
2025-07-11 00:45:58 +0800

    Added working Flowise chatbot integration

---
+++ static/js/flowise-web.js
@@ -0,0 +1,5 @@
+(function(w,d,s,u,f,n){
+  w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments)};
+  n=d.createElement(s);n.async=1;n.src='/static/js/flowise-chatbot.min.js';
+  d.head.appendChild(n)
+})(window,document,'script','','initFlowiseChatbot');
